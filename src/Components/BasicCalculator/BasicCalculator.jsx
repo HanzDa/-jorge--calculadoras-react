08716dcc813584bde3f21c993a95de2396935409
Jorge Andres Riaño <jarps1994@gmail.com>
2024-05-30 21:37:47 -0500

    calculadora casi terminada, todavia no da el resultado

--- src/Components/BasicCalculator/BasicCalculator.jsx
+++ src/Components/BasicCalculator/BasicCalculator.jsx
@@ -3,31 +3,44 @@ import React from 'react';
 import styles from './basic.module.css';
 import { useState } from 'react';
 const BasicCalculator = () => {
-  const [inputValue, setInputValue] = useState(''); //--> primer valor 
-  const [secondValue, setSecondValue] = useState(''); //--> segundo valor 
-
+  const [inputValue, setInputValue] = useState(""); //--> primer valor 
+  const [secondValue, setSecondValue] = useState(""); //--> segundo valor 
+  const [operator, setOperator] = useState(""); //--> EL operador que se hara
   const handleButtonClick = (value) => {
     setInputValue(inputValue + value);
   };
 
-  const handleMultiply = () => {
-    resultMultiply = inputValue * secondValue;
-    ;
+  const handleOperatorClick = (op) => {
+    setOperator(op);
+    setInputValue("");
+    setSecondValue(secondValue);
   };
 
-  const handleDivide = () => {
-    resultDivide = inputValue / secondValue
-  }
 
-  const handleSum = () => {
-    resultSum = inputValue + secondValue
+  const handleResults = () => {
+    let result = 0;
+    switch (operator) {
+      case "+":
+        result = parseFloat(inputValue) + parseFloat(secondValue);
+        break;
+      case "-":
+        result = parseFloat(inputValue) - parseFloat(secondValue);
+        break;
+      case "*":
+        result = parseFloat(inputValue) * parseFloat(secondValue);
+        break;
+      case "/":
+        if (parseFloat(inputValue) !== 0) {
+          result = parseFloat(secondValue) / parseFloat(inputValue);
+        } else {
+          result = "error";
+        }
+        break;
+      default:
+        break;
+    }
   };
-
-  const handleRest = () => {
-    resultReset = inputValue - secondValue
-  }
-
-
+  
 
 
 
@@ -36,14 +49,15 @@ const BasicCalculator = () => {
     setInputValue('');
   }
 
+  // el onchange sirve para borrar en el input. para modificarlo
   return (
     <div className={styles.calculator}>
-      <input type="text" value={inputValue} />
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
       <br></br>
       <div className={styles.button}>
-        <button onClick={() => handleButtonClick(1)}>1</button>
-        <button onClick={() => handleButtonClick(2)} >2</button>
-        <button onClick={() => handleButtonClick(3)}>3</button>
+        <button onClick={() => handleButtonClick("1")}>1</button>
+        <button onClick={() => handleButtonClick("2")} >2</button>
+        <button onClick={() => handleButtonClick("3")}>3</button>
       </div>
       <div className={styles.button}>
         <button onClick={() => handleButtonClick("4")} >4</button>
@@ -56,15 +70,15 @@ const BasicCalculator = () => {
         <button onClick={() => handleButtonClick("9")}>9</button>
       </div>
       <div className={styles.button}>
-        <button>.</button>
+        <button onClick={() => handleButtonClick(".")}>.</button>
         <button onClick={() => handleButtonClick("0")}>0</button>
-        <button>=</button>
+        <button onClick={handleResults}>=</button>
       </div>
       <div className={styles.button}>
-        <button onClick={handleDivide}>/</button>
-        <button onClick={handleMultiply}>*</button>
-        <button onClick={handleRest}>-</button>
-        <button onClick={handleSum}>+</button>
+        <button onClick={handleOperatorClick}>/</button>
+        <button onClick={handleOperatorClick}>*</button>
+        <button onClick={handleOperatorClick}>-</button>
+        <button onClick={handleOperatorClick}>+</button>
         <button onClick={handleReset}>C</button>
       </div>
     </div>
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,8 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
  const propertiesReducer = createSlice({
     name: "properties",
     initialState: [],
-    // reducers: {
-    // },
+    reducers: {
+    },
 
 }); 
 
